775b9bce34a0d0f9f902bf4f8dcb8a283afaafac
Byron Boudreaux <phydeauxman@users.noreply.github.com>
2018-02-19 19:47:58 -0500

    Change name of backend.tf

--- terraform-azure/images.tf
+++ terraform-azure/images.tf
@@ -2,11 +2,11 @@
 # https://github.com/terraform-providers/terraform-provider-azurerm/issues/577
 
 data "azurerm_image" "elasticsearch" {
-  name                = "elasticsearch5-2017-11-17T013212"
+  name                = "elasticsearch5-2018-02-11T105507"
   resource_group_name = "packer-elasticsearch-images"
 }
 
 data "azurerm_image" "kibana" {
-  name                = "kibana5-2017-11-23T074321"
+  name                = "kibana5-2018-02-15T111136"
   resource_group_name = "packer-elasticsearch-images"
 }
